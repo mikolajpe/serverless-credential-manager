f18276cf2c703b5731fa94b4a1250c4f99f4e916
Mikolaj Przybysz <mikolaj.przybysz@home24.de>
2016-05-18 19:21:45 +0200

    added implementation code and readme

---
+++ README.md
@@ -0,0 +1,47 @@
+Serverless Credential Manager
+=============================
+
+This plugin does add Cli option, that sets credentials in `admin.env` file, which is useful when you clone
+existing project, and you need to set credentials in order to deploy project to either dev or live environment. 
+
+```
+$ sls credential set [-s <stage>] [-p <profile>]
+```
+
+Both parameters are optional. If not set interactive mod will kick in to let you select stage (list of stages 
+will be generated based on contents of your `\_meta` folder) and profile (list of profiles will be generated 
+based on `~/.aws/credentials` file contents).
+
+### Prerequisites
+
+* aws credentials
+* serverless project
+
+### Installation
+
+```
+cd myproject
+npm install serverless-credential-manager
+```
+
+Add the plugin to your project by doing following modification:
+
+File: *s-project.json*
+```
+{
+    ...
+    "plugins": [
+        "serverless-credential-manager"
+    ]
+    ...
+}
+```
+
+### Sample Usage
+
+```
+$ git clone git@github.com:myuserid/myprojectwithmetaconfiguration.git
+$ sls credential set -s livestage -p liveprofile \
+  && sls function deploy -s livestage -r my-region-1 -a \
+  && sls endpoint deploy -s livestage -r my-region-1 -a
+```
--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 module.exports = function(S) {
 
     const path    = require( 'path' ),
+        SUtils    = S.utils,
         SError    = require(S.getServerlessPath('Error')),
-        SCli      = require(S.getServerlessPath('utils/cli')),
-        BbPromise = require( 'bluebird' );
+        BbPromise = require( 'bluebird'),
+        os        = require('os'),
+        fs        = BbPromise.promisifyAll(require('fs'));
 
     class CredentialManager extends S.classes.Plugin {
 
@@ -30,11 +32,11 @@ module.exports = function(S) {
                     {
                         option:      'stage',
                         shortcut:    's',
-                        description: 'Required - Serverless stage for which set credential'
+                        description: 'Optional - Serverless stage for which set credential'
                     }, {
                         option:      'profile',
                         shortcut:    'p',
-                        description: 'Required - AWS Credential profile to be set for stage'
+                        description: 'Optional - AWS Credential profile to be set for stage'
                     }
                 ]
             });
@@ -44,10 +46,89 @@ module.exports = function(S) {
         /**
          * Action
          */
-        credentialManager() {
+        credentialManager(evt) {
+
+            let _this = this;
+            _this.evt = evt;
+            _this.provider = S.getProvider();
+            _this.project  = S.getProject();
+
+            _this.evt.options.stage   = _this.evt.options.stage ? _this.evt.options.stage : null;
+            _this.evt.options.profile = _this.evt.options.profile ? _this.evt.options.profile : null;
+
+            // Flow
+            return BbPromise.try(function() {})
+                .bind(_this)
+                .then(function() {
+                    return _this.cliPromptSelectStage('Choose a Stage: ', _this.evt.options.stage, false)
+                        .then(stage => {
+                        _this.evt.options.stage = stage;
+                        })
+                })
+                .then(function() {
+
+                    // Skip if user just made a new profile
+                    if (_this.evt.options.profile) return;
+
+                    // Select A Profile
+                    _this.profiles = _this.provider.getAllProfiles();
+
+                    if (!_this.profiles) throw new SError(`You have no profiles for ${_this.provider.getProviderName()} on this machine.  Please re-run this command and create a new profile.`);
+
+                    let profileNameList = Object.keys(_this.profiles);
+
+                    // if private has 1 stage, skip prompt
+                    if (profileNameList.length === 1) {
+                        return BbPromise.resolve(profileNameList[0]);
+                    }
+
+                    // Prompt: profile select
+                    let choices = [];
+                    for (let i = 0; i < profileNameList.length; i++) {
+                        choices.push({
+                            key: (i + 1) + ') ',
+                            value: profileNameList[i],
+                            label: profileNameList[i]
+                        });
+                    }
+
+                    return _this.cliPromptSelect('Select a profile for your project: ', choices, false)
+                        .then(profile => {
+                        _this.evt.options.profile = profile[0].value;
+                        })
+                })
+                .then(_this._setCredentials)
+        }
+
+        _setCredentials() {
 
             let _this = this;
 
+            return new BbPromise(function(resolve, reject) {
+
+                // Check Params
+                if (!_this.evt.options.stage) {
+                    return BbPromise.reject(new SError('Missing stage option'));
+                }
+
+                // Check Params
+                if (!_this.evt.options.profile) {
+                    return BbPromise.reject(new SError('Missing profile option'));
+                }
+
+                // Write to admin.env
+                let adminEnv = _this.project.getRootPath('admin.env'),
+                    profileEnvVar = 'AWS_' + _this.evt.options.stage.toUpperCase() + '_PROFILE';
+                if (SUtils.fileExistsSync(adminEnv)) {
+                    // Append to admin.env
+                    fs.appendFileSync(adminEnv, os.EOL + `${profileEnvVar}=${_this.evt.options.profile}`);
+                } else {
+                    // Create admin.env
+                    SUtils.writeFileSync(adminEnv, `${profileEnvVar}=${_this.evt.options.profile}`);
+                }
+
+                return BbPromise.resolve();
+            });
         }
     }
 
